3d04dde2d4bcc687ee14cdde1448d7a47d7c7c06
tinarskii <tinvv@outlook.co.th>
2025-05-09 01:01:37 +0700

    feat: improve song-queue command

--- client/commands/song-queue.ts
+++ client/commands/song-queue.ts
@@ -7,6 +7,11 @@ export default {
   name: "song-queue",
   description: "Check song queue",
   alias: ["queue", "sq"],
+  args: [{
+    name: "page",
+    description: "Page number",
+    required: false,
+  }],
   execute: async (
     client: { api: ApiClient; chat: ChatClient; io: any },
     meta: {
@@ -24,9 +29,34 @@ export default {
       return;
     }
 
-    await client.chat.say(
-      meta.channel,
-      `@${meta.user} ดูคิวเพลงได้ที่ https://my.tinarskii.com:3000/queue นะคะ!`,
-    );
-  },
-};
+    const page = parseInt(args[0]) || 1;
+    const charactersPerPage = 500;
+    const itemsPerPage = 3;
+    let msg = `[หน้า ${page}/${Math.ceil(songQueue.length / itemsPerPage)}] `;
+
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const songs = songQueue.slice(start, end);
+    const songList = songs
+      .map((song, index) => {
+        const songIndex = start + index + 1;
+        if (songIndex === 1) return;
+        return `${songIndex - 1}. ${song.song.title} (${song.user})`;
+      })
+      .join(" | ");
+
+    msg += songList;
+
+    // Check if there are more songs
+    if (songQueue.length > end) {
+      msg += ` (...และอีก ${songQueue.length - end} เพลง)`;
+    }
+
+    // Check if the message exceeds the character limit
+    if (msg.length > charactersPerPage) {
+      msg = msg.slice(0, charactersPerPage - 3) + "...";
+    }
+
+    await client.chat.say(meta.channel, msg);
+  }
+}
