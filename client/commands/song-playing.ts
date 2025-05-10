ebdd18399d4bd44c9709a0d014a8113187e19369
tinarskii <tinvv@outlook.co.th>
2025-05-11 00:43:34 +0700

    fix(bot): song-playing command shows incorrect queue length

--- client/commands/song-playing.ts
+++ client/commands/song-playing.ts
@@ -32,7 +32,7 @@ export default {
 
     await client.chat.say(
       meta.channel,
-      `@${meta.user} ตอนนี้กำลังเล่น "${songTitle}" โดย ${songUser} — ${songAuthor} รีเควส ((${songQueue.length === 0 ? 'ไม่มีเพลงในคิว' : `มี ${songQueue.length} เพลงในคิว`}))`,
+      `@${meta.user} ตอนนี้กำลังเล่น "${songTitle}" โดย ${songUser} — ${songAuthor} รีเควส (${songQueue.length - 1 === 0 ? 'ไม่มีเพลงในคิว' : `มี ${songQueue.length - 1} เพลงในคิว`})`,
     );
   },
 };
